16f9ec9f62a15315cf7be731063ac667bea459e0
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-04-30 16:47:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getFormvalue() {
-    //Write your code here
-
+	let first = document.getElementById("fname").value;
+	let last = document.getElementById("lname").value;
+	alert(first+" "+last);
 }
